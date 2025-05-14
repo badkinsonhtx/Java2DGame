3a83cc71cffa55825d74d8c249af09ced3e18664
badkinsonhtx <41838883+badkinsonhtx@users.noreply.github.com>
2025-05-14 16:50:09 -0500

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -23,6 +24,8 @@ public class Player extends Entity {
 		
 		screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
 		screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+		
+		solidArea = new Rectangle(8, 16, 32, 32);
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -55,17 +58,38 @@ public class Player extends Entity {
 		keyH.leftPressed == true || keyH.rightPressed == true) {
 			if(keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			}else if(keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			}else if(keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			}else if(keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
 			}
+			
+			// Check Tile Collision
+			collisionOn = false;
+			gp.cc.checkTile(this);
+			
+			// If Collision Is False, Player Can Move
+			if(collisionOn == false) {
+				switch(direction) {
+				case "up":
+					worldY -= speed;
+					break;
+                case "down":
+                	worldY += speed;
+					break;
+                case "left":
+                	worldX -= speed;
+	                break;
+                case "right":
+                	worldX += speed;
+	                break;
+	            default:
+	            	break;
+				}
+			}
+			
 			spriteCounter++;
 			if(spriteCounter > 12) {
 				if(spriteNum == 1) {
